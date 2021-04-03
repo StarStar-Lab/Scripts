8669fa528ca7bbfd9c531abde7a46f6125a7031b
StarStar-Lab <35412541+StarStar-Lab@users.noreply.github.com>
2021-04-03 15:24:25 +0800

    Update sunert_list.sh

--- sunert_list.sh
+++ sunert_list.sh
@@ -1,3 +1,4 @@
 */15 * * * * sleep \$((RANDOM % 120)); node /scripts/youth.js |ts >> /logs/youth.log 2>&1
 */7 * * * * sleep \$((RANDOM % 120)); node /scripts/txnews.js |ts >> /logs/txnews.log 2>&1
 10 7-22 * * * sleep \$((RANDOM % 120)); node /scripts/baidu_speed.js >> /logs/baidu_speed.log 2>&1
+0 6 * * * node /scripts/baidu_speed.js >> /logs/baidu_speed.log 2>&1
