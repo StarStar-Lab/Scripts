4ba90673ff672c6fc6ffb7bca00012d1f050aa25
StarStar-Lab <35412541+StarStar-Lab@users.noreply.github.com>
2021-04-04 09:41:36 +0800

    Update and rename sunert_list.sh to mod_list.sh

--- mod_list.sh
+++ mod_list.sh
@@ -2,3 +2,5 @@
 */7 * * * * sleep \$((RANDOM % 120)); node /scripts/txnews.js |ts >> /scripts/logs/txnews.log 2>&1
 10 7-22 * * * sleep \$((RANDOM % 120)); node /scripts/baidu_speed.js >> /scripts/logs/baidu_speed.log 2>&1
 0 6 * * * node /scripts/baidu_speed.js >> /scripts/logs/baidu_speed.log 2>&1
+5,35 8-23 * * * node /scripts/jrtt.js |ts >> /scripts/logs/jrtt.log 2>&1
+0,30 * * * * node /scripts/qczjspeed.js |ts >> /scripts/logs/qczjspeed.log 2>&1
