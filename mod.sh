c0759be4079f2d088000009e46e35047cca9da80
StarStar-Lab <35412541+StarStar-Lab@users.noreply.github.com>
2021-04-11 16:18:52 +0800

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -33,4 +33,5 @@ echo "get single scripts"
 cd /scripts/logs
 wget -O jrtt.js https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/jrtt.js
 wget -O qczjspeed.js https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/qczjspeed.js
+cp -f /scripts/logs/*.js /scripts
 echo "done"
