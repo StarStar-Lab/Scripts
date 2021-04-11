d5c8c26362a776d6fac58d8b92963e3977c33dcb
StarStar-Lab <35412541+StarStar-Lab@users.noreply.github.com>
2021-04-11 20:34:59 +0800

    Update mod_list.sh

--- mod_list.sh
+++ mod_list.sh
@@ -1,5 +1,5 @@
-*/15 * * * * node /scripts/youth.js |ts >> /scripts/logs/youth.log 2>&1
-*/7 * * * * node /scripts/txnews.js |ts >> /scripts/logs/txnews.log 2>&1
+*/11 0-23 * * * node /scripts/youth.js |ts >> /scripts/logs/youth.log 2>&1
+*/7 0-22 * * * node /scripts/txnews.js |ts >> /scripts/logs/txnews.log 2>&1
 10 7-22 * * * node /scripts/baidu_speed.js >> /scripts/logs/baidu_speed.log 2>&1
 0 6 * * * node /scripts/baidu_speed.js >> /scripts/logs/baidu_speed.log 2>&1
 5,35 8-23 * * * node /scripts/jrtt.js |ts >> /scripts/logs/jrtt.log 2>&1
